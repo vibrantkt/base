0b32919addf954806b27b81e59adce5c4f314614
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-26 23:11:42 +0300

    f**king generics

--- src/main/kotlin/org/vibrant/base/database/blockchain/InstantiateBlockChain.kt
+++ src/main/kotlin/org/vibrant/base/database/blockchain/InstantiateBlockChain.kt
@@ -4,5 +4,5 @@ import org.vibrant.base.database.blockchain.models.BlockChainModel
 import org.vibrant.base.database.blockchain.models.BlockModel
 
 interface InstantiateBlockChain<B: BlockModel, T: BlockChainModel> {
-    fun asBlockChainProducer(model: B): BlockChain<B, T>
+    fun asBlockChainProducer(model: T): BlockChain<B, T>
 }
