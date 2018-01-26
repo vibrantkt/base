ddc44465987cffa78cc9d1d5ea357c238ca0531b
enchantinggg4 <enchantinggg4@gmail.com>
2018-01-26 22:59:40 +0300

    whoops again generics

--- src/main/kotlin/org/vibrant/base/database/blockchain/InMemoryBlockChain.kt
+++ src/main/kotlin/org/vibrant/base/database/blockchain/InMemoryBlockChain.kt
@@ -7,7 +7,7 @@ import org.vibrant.base.database.blockchain.models.BlockModel
 /**
  * @property blocks Ordered list of blocks from genesis to the last
  */
-abstract class InMemoryBlockChain<B: BlockModel, out T: BlockChainModel>: BlockChain<B, T>() {
+abstract class InMemoryBlockChain<B: BlockModel, T: BlockChainModel>: BlockChain<B, T>() {
 
     protected val blocks = arrayListOf(this.createGenesisBlock())
 
